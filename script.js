24de98874063d6e633d635c15578f33383b0c6d6
Yagna17 <syagna3131@gmail.com>
2024-11-13 13:41:33 +0530

    all files added with style submodule

--- script.js
+++ script.js
@@ -1,10 +1,3 @@
-// Dynamically set CSS based on branch
-const link = document.getElementById("car-style");
-if (window.location.href.includes("style-one")) {
-    link.href = "./styles/style1.css";
-} else if (window.location.href.includes("style-two")) {
-    link.href = "./styles/style2.css";
-}
 //shows the page 
 function showPage(pageNumber) {
     document.querySelectorAll('.car-page').forEach(page => page.style.display = 'none');
